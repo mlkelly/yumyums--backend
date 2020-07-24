5ea17ee52b1dd39a9574c1829ee89ed20eb3ba15
mlkelly <maurak87@gmail.com>
2020-07-24 09:10:59 -0400

    seed data created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# if have estra time srape recipes from api
 
-# dont' froget to destroy_all
-# start with the ones that belogns_to and only afterwards add has_many to avoid multiplications in database
- 
-# "ClassName.reset_pk_sequence" allows you to start fresh ids whenever you run rails db:seed
+classes = [Ingredient, User, Recipe, Review, RecIngred]
+classes.each{|c|
+    c.destroy_all 
+    c.reset_pk_sequence #resets PKs when run rails db:seed
+}
 
+i1 = Ingredient.create(name: "Butter")
+i2 = Ingredient.create(name: "Sourdough Bread")
+i3 = Ingredient.create(name: "Cheddar Cheese")
+i4 = Ingredient.create(name: "Truffle Mushrooms")
+i5 = Ingredient.create(name: "Onion")
+
+u1 = User.create(username: "mlk1000", bio:"Food artist", img: "https://www.sackettwaconia.com/wp-content/uploads/default-profile.png")
+
+recipe1 = Recipe.create(title:"Truffle Grilled Cheese", abt: "Quick and Easy Twist on the Classic Grilled Cheese", user_id: u1.id)
+
+rec1 = RecIngred.create(amount:"2 tbsp", instruction:"Heartily apply to both sides of each slice", recipe_id: recipe1.id,ingredient_id: i1.id)
+rec2 = RecIngred.create(amount:"2 slices", instruction:"Once buttered toast up on the frying pan", recipe_id: recipe1.id,ingredient_id: i2.id)
+rec3 = RecIngred.create(amount:"2 slices", instruction:"Put one slice on each piece of bread", recipe_id: recipe1.id,ingredient_id: i3.id)
+rec4 = RecIngred.create(amount:"1/8 cup", instruction:"Place in between the two slices of cheese", recipe_id: recipe1.id,ingredient_id: i4.id)
+rec5 = RecIngred.create(amount:"1/8 cup diced", instruction:"Sautee until translucent, add to the middle of the sandwich with mushrooms", recipe_id: recipe1.id,ingredient_id: i5.id)
+
+review1 = Review.create(rating: 4,reason: "Really great taste, but too many onions, not enough cheese", user_id: u1.id, recipe_id: recipe1.id)
